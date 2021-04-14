a8a90cf583946847063cb4e132d00f00e64c43e8
harshva89 <harshvaswani89@gmail.com>
2021-04-14 17:38:28 +0530

    ready

--- app/src/main/java/com/example/garbageiot/MainActivity.java
+++ app/src/main/java/com/example/garbageiot/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.garbageiot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new CountDownTimer(10000,1000)
+        {
+            public void onTick(long milliseconds)
+            {
+
+            }
+            public void onFinish()
+            {
+                Intent i = new Intent(MainActivity.this, LandingPage.class);
+                startActivity(i);
+            }
+
+        }.start();
     }
 }

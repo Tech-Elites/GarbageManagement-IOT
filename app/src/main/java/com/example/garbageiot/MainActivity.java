b01615e6940af3c4281a66c7e9fd7f5224183c50
harshva89 <harshvaswani89@gmail.com>
2021-04-14 18:08:31 +0530

    base ready

--- app/src/main/java/com/example/garbageiot/MainActivity.java
+++ app/src/main/java/com/example/garbageiot/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new CountDownTimer(10000,1000)
+        new CountDownTimer(3000,1000)
         {
             public void onTick(long milliseconds)
             {
